2b6f193e5bfa875b8c77ab3c953e6ca78ee635c8
Matthew Zygowicz <ziggy@MacBook-Pro.localdomain>
2020-10-25 18:56:02 -0500

    feat: simulate a more complex route

--- __tests__/route.spec.js
+++ __tests__/route.spec.js
@@ -5,25 +5,48 @@
  *
  * I prefer using nock for mocking service requests as it closer simulates the call stack performed by axios.
  */
-const nock = require('nock')
-const request = require('supertest')
-const app = require('../src/app')
+const nock = require("nock");
+const request = require("supertest");
+const app = require("../src/app");
+const mockLdap = require("../src/ldap/client");
 
-describe('post-root tests', () => {
-  it('should call getAllFilms, taking the first result and passing it to getFilmById', (done) => {
-    const mockItem = { id: '321' }
-    nock('https://ghibliapi.herokuapp.com')
+jest.mock("../src/ldap/client", () => {
+  return {
+    bind: jest.fn(),
+    unbind: jest.fn(),
+    search: jest.fn(),
+  };
+});
+
+describe("post-root tests", () => {
+  afterEach(() => {
+    mockLdap.search.mockReset();
+    mockLdap.bind.mockReset();
+    mockLdap.unbind.mockReset();
+  });
+  it("should call getAllFilms, taking the first result and passing it to getFilmById", (done) => {
+    const mockItem = { id: "321" };
+    mockLdap.search.mockImplementation(() => {
+      return { searchEntries: "matt@zygowicz.com" };
+    });
+    nock("https://ghibliapi.herokuapp.com")
       .get(`/films`)
-      .reply(200, [mockItem, { id: '123' }])
-    nock('https://ghibliapi.herokuapp.com').get(`/films/${mockItem.id}`).reply(200, mockItem)
+      .reply(200, [mockItem, { id: "123" }]);
+    nock("https://ghibliapi.herokuapp.com")
+      .get(`/films/${mockItem.id}`)
+      .reply(200, mockItem);
     request(app)
-      .get('/enterprise-testing-patterns')
+      .post("/enterprise-testing-patterns")
       .expect(200)
-      .end((err) => {
+      .end((err, res) => {
         if (err) {
-          done(err)
+          done(err);
         }
-        done()
-      })
-  })
-})
+        expect(res.body).toEqual({
+          film: { id: "321" },
+          user: "matt@zygowicz.com",
+        });
+        done();
+      });
+  });
+});
--- src/routes/post-root.js
+++ src/routes/post-root.js
@@ -1,18 +1,19 @@
-const services = require('../services')
+const services = require("../services");
+const { getUsers } = require("../ldap/ldap-calls");
 
 const postRoot = async (req, res) => {
-  const { body, logger } = req
-  logger.info({ body }, 'Request Received')
+  const { body, logger } = req;
+  logger.info({ body }, "Request Received");
 
   try {
-    const films = await services.getAllFIlms(logger)
-    const film = await services.getByFilmId(films[0].id, logger)
-
-    res.status(200).json(film)
+    const films = await services.getAllFIlms(logger);
+    const film = await services.getByFilmId(films[0].id, logger);
+    const user = await getUsers("matt@zygowicz.com");
+    res.status(200).json({ film, user });
   } catch (e) {
-    logger.error('caught error')
-    res.status(500).json({ error: true })
+    logger.error("caught error");
+    res.status(500).json({ error: true });
   }
-}
+};
 
-module.exports = postRoot
+module.exports = postRoot;
